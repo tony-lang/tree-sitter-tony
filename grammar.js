0539d4507e8bb2a63f971111dea75d2427b6b01b
Jonas Hübotter <jonas.huebotter@gmail.com>
2020-03-24 10:48:41 +0100

    [#73] Type hints (#79)

--- grammar.js
+++ grammar.js
@@ -282,6 +282,8 @@ module.exports = grammar({
         alias($.identifier, $.identifier_pattern),
         $._destructuring_pattern
       )),
+      '::',
+      field('type', $.type),
       ':=',
       field('right', $._simple_expression)
     ),
@@ -290,6 +292,8 @@ module.exports = grammar({
         alias($.identifier, $.identifier_pattern),
         $._destructuring_pattern
       )),
+      '::',
+      field('type', $.type),
       ':=',
       choice(
         field('right', $._compound_expression),
@@ -381,6 +385,7 @@ module.exports = grammar({
     ),
     generator_condition: $ => seq('if', $._simple_expression),
 
+    type: $ => /_?[A-Z0-9][a-zA-Z0-9]*/,
     _identifier_without_operators: $ => /[a-z_][a-z0-9_]*\??/,
     _operator: $ => choice(/(==|[!@$%^&*|<>~*\\\-+/.]+)=*>?/, '/'),
     identifier: $ => choice($._operator, $._identifier_without_operators),
