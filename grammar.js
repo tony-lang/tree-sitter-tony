c7fb09f8a51fcc38be16e0fe58de6368d63bec41
Jonas Hübotter <jonas.huebotter@gmail.com>
2020-03-30 09:32:33 +0200

    [#133] Remove rest_list_type

--- grammar.js
+++ grammar.js
@@ -1,4 +1,5 @@
 const PREC = Object.freeze({
+  PARAMETERS: -2,
   REGEX: -1,
   NAMED_INFIX: 1,
   OPERATOR_INFIX: 2,
@@ -224,7 +225,7 @@ module.exports = grammar({
       optional(seq('::', field('type', $.type_constructor)))
     ),
 
-    parameters: $ => prec(-1, choice(
+    parameters: $ => prec(PREC.PARAMETERS, choice(
       commaSep1($.pattern),
       seq('(', ')')
     )),
@@ -444,14 +445,12 @@ module.exports = grammar({
       $.type,
       $.map_type,
       $.tuple_type,
-      $.list_type,
-      $.rest_list_type
+      $.list_type
     ),
     _type_group: $ => seq('(', $.type_constructor, ')'),
     map_type: $ => seq('{', field('left', $.type_constructor), ':', field('right', $.type_constructor), '}'),
     tuple_type: $ => seq('(', commaSep2($.type_constructor), ')'),
     list_type: $ => seq('[', field('type', $.type_constructor), ']'),
-    rest_list_type: $ => seq('...', field('type', $.list_type)),
 
     _identifier_without_operators: $ => /[a-z][a-z0-9_]*\??/,
     _operator: $ => /[!@$%^&*|<>~*\\\-+.=]+/,
