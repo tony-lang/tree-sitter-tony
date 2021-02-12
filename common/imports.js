b47dc21f0e70f4f943c23e9b907b110833943335
Jonas Hübotter <jonas.huebotter@gmail.com>
2021-02-12 09:15:10 +0100

    [#389] Change default import syntax

--- common/imports.js
+++ common/imports.js
@@ -18,7 +18,7 @@ module.exports = {
                       'default',
                       alias($.identifier, $.identifier_pattern_name),
                     ),
-                    ',',
+                    '@',
                   ),
                 ),
                 '{',
