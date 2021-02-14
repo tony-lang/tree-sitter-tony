ceca967224bbc3e7fdd822f169388d08f2858455
Jonas Hübotter <jonas.huebotter@gmail.com>
2021-02-14 12:16:14 +0100

    declare constants types

---
+++ common/constants.d.ts
@@ -0,0 +1,10 @@
+// numbers
+export const DIGITS: RegExp
+export const EXP: RegExp
+export const HEX: RegExp
+export const OCT: RegExp
+export const BIN: RegExp
+
+// identifiers
+export const IDENTIFIER: RegExp
+export const OPERATOR: RegExp
