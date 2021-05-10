450a41460528cdafaa8a3fc6befe7c948783d4bf
Jonas Hübotter <jonas.huebotter@gmail.com>
2021-05-10 14:10:12 +0200

    fix

--- common/imports.ts
+++ common/imports.ts
@@ -8,52 +8,50 @@ export const exported_import = <RuleName extends string>(
   $: GrammarSymbols<RuleName>,
 ) => seq('export', $._import_body)
 
-export const _import_body_constructor = (dialect: Dialect) => <
-  RuleName extends string
->(
-  $: GrammarSymbols<RuleName>,
-) => {
-  switch (dialect) {
-    case Dialect.DTN:
-      return prec.left(
-        Prec.Pattern,
-        seq(
-          '{',
-          commaSep1(field('import', $.import_type)),
-          '}',
-          'from',
-          field('source', $.raw_string),
-        ),
-      )
-    case Dialect.Tony:
-      return prec.left(
-        Prec.Pattern,
-        seq(
-          choice(
-            field('default', alias($.identifier, $.identifier_pattern_name)),
-            seq(
-              optional(
-                seq(
-                  field(
-                    'default',
-                    alias($.identifier, $.identifier_pattern_name),
-                  ),
-                  ',',
-                ),
-              ),
-              '{',
-              commaSep1(
-                field('import', choice($.import_identifier, $.import_type)),
-              ),
-              '}',
-            ),
+export const _import_body_constructor =
+  (dialect: Dialect) =>
+  <RuleName extends string>($: GrammarSymbols<RuleName>) => {
+    switch (dialect) {
+      case Dialect.DTN:
+        return prec.left(
+          Prec.Pattern,
+          seq(
+            '{',
+            commaSep1(field('import', $.import_type)),
+            '}',
+            'from',
+            field('source', $.raw_string),
           ),
-          'from',
-          field('source', $.raw_string),
-        ),
-      )
+        )
+      case Dialect.Tony:
+        return prec.left(
+          Prec.Pattern,
+          seq(
+            choice(
+              field('default', alias($.identifier, $.identifier_pattern_name)),
+              seq(
+                optional(
+                  seq(
+                    field(
+                      'default',
+                      alias($.identifier, $.identifier_pattern_name),
+                    ),
+                    ',',
+                  ),
+                ),
+                '{',
+                commaSep1(
+                  field('import', choice($.import_identifier, $.import_type)),
+                ),
+                '}',
+              ),
+            ),
+            'from',
+            field('source', $.raw_string),
+          ),
+        )
+    }
   }
-}
 
 export const import_identifier = <RuleName extends string>(
   $: GrammarSymbols<RuleName>,
